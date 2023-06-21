ca229a71ab7ea3c75d91cea09daf038601c5578d
Hewerton Adao da Paz <hewerton.paz@compasso.com.br>
2023-06-21 14:43:57 -0300

    add generated prisma files

--- src/modules/author/author.model.ts
+++ src/modules/author/author.model.ts
@@ -1,7 +1,7 @@
 import { Pagination } from '../../common/resolvers/pagination.resolver'
 import { Author } from '../../prisma/generated/type-graphql'
 import { Field, ObjectType } from 'type-graphql'
-import { Author as PrismaAuthor } from '../../prisma/generated/type-prisma'
+import { Author as PrismaAuthor } from '@prisma/client'
 
 @ObjectType()
 export class PaginedAuthor extends Pagination {
--- src/modules/song/song.model.ts
+++ src/modules/song/song.model.ts
@@ -1,7 +1,7 @@
 import { Pagination } from '../../common/resolvers/pagination.resolver'
 import { Song } from '../../prisma/generated/type-graphql'
 import { Field, ObjectType } from 'type-graphql'
-import { Song as PrismaSong } from '../../prisma/generated/type-prisma'
+import { Song as PrismaSong } from '@prisma/client'
 
 @ObjectType()
 export class PaginedSong extends Pagination {
